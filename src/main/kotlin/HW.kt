611d58e43ffeeb63a76b36c3cb4fb925af06fc29
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-07-06 10:39:12 +0200

    comment out line

--- src/main/kotlin/HW.kt
+++ src/main/kotlin/HW.kt
@@ -1,4 +1,5 @@
 fun main(args:Array<String>) {
     // This function prints out a message
-   // Cool line println("Hello Bulcsú!") // This is great line, please don't change it
+   // println("Hello Bulcsú!")
+    println("Hello world!")
 }
