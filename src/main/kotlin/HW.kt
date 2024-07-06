0cd48da74c26f88a6f7645383410bbb982c51808
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-07-06 10:37:37 +0200

    add single line before and after the code

--- src/main/kotlin/HW.kt
+++ src/main/kotlin/HW.kt
@@ -1,4 +1,4 @@
 fun main(args:Array<String>) {
     // This function prints out a message
-    println("Hello Bulcsú!")
+   // Cool line println("Hello Bulcsú!") // This is great line, please don't change it
 }
