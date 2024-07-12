2f5bf2521ecf5a97a3bbcbc10fdcbf8aceb5e5fe
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-07-12 21:46:51 +0200

    showcas number types

---
+++ src/main/kotlin/s6-numbers-variable-types/NumberTypes.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>) {
+    var items = 5
+    println(items::class.java)
+
+    var nbOfPeople = 7500000000
+    println(nbOfPeople::class.java)
+
+    var prince = 29.99
+    println(prince::class.java)
+
+    var pi = 3.1415798423423
+    println(pi::class.java)
+}
+
