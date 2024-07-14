e9a8c4b3ccf7859065dececc03c35122d8be21b9
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-07-14 21:39:26 +0200

    Practice int and long

--- src/main/kotlin/s6-numbers-variable-types/Practice53.kt
+++ src/main/kotlin/s6-numbers-variable-types/Practice53.kt
@@ -1,11 +1,17 @@
 fun main(args:Array<String>) {
     println("Please enter a number")
     val numberInput = readLine()?:"0"
-    val myByte = numberInput.toByte()
-    println(myByte)
-    println(myByte::class.java)
+//    val myByte = numberInput.toByte()
+//    println(myByte)
+//    println(myByte::class.java)
+//
+//    val myShort = myByte.toShort()
+//    println(myShort::class.java)
 
-    val myShort = myByte.toShort()
-    println(myShort::class.java)
+//    val myInt = numberInput.toInt()
+//    println(myInt::class.java)
+//
+//    val myLong = numberInput.toLong()
+//    println(myLong::class.java)
 }
 
