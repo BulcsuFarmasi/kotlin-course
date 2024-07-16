2a37782754a0510c7eebc46151394675ebc6b177
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-07-16 20:42:32 +0200

    practice double conversions

--- src/main/kotlin/s6-numbers-variable-types/Practice53.kt
+++ src/main/kotlin/s6-numbers-variable-types/Practice53.kt
@@ -1,6 +1,6 @@
 fun main(args:Array<String>) {
-    println("Please enter a number")
-    val numberInput = readLine()?:"0"
+ //   println("Please enter a number")
+//    val numberInput = readLine()?:"0"
 //    val myByte = numberInput.toByte()
 //    println(myByte)
 //    println(myByte::class.java)
@@ -13,5 +13,17 @@ fun main(args:Array<String>) {
 //
 //    val myLong = numberInput.toLong()
 //    println(myLong::class.java)
+
+    val pi = 3.1415
+    println("$pi is a ${pi::class.java}")
+
+    val piFloat = pi.toFloat()
+    println("$piFloat is a ${piFloat::class.java}")
+
+    val piInt = pi.toInt()
+    println("$piInt is an ${piInt::class.java}")
+
+    val piString = pi.toString()
+    println("$piString is a ${piString::class.java}")
 }
 
