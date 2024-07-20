1525e51814fb4c43e9b5076ad513106b629e8213
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-07-20 13:01:17 +0200

    result typea

---
+++ src/main/kotlin/s7-operators-booleans/ResultType.kt
@@ -0,0 +1,16 @@
+fun main(args:Array<String>) {
+//    val a:Short = 6
+//    val b:Int = 32
+//
+//    val result = a * b
+//
+//    println(result::class.java)
+
+    val a:Long = 28645826
+    val b: Float = 13.67F
+
+    val result =  a / b
+
+    println(result::class.java)
+}
+
