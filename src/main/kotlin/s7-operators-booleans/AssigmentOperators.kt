225611b2e8a3cab4f5347f7c43855f697054bfe0
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-07-30 18:19:18 +0200

    reference operator

--- src/main/kotlin/s7-operators-booleans/AssigmentOperators.kt
+++ src/main/kotlin/s7-operators-booleans/AssigmentOperators.kt
@@ -2,5 +2,9 @@ fun main(args:Array<String>) {
     val dozen = 12
     var months = dozen
     println(months)
+
+    val userName = "Lucy"
+    val greeting = "Hello, $userName"
+    println(greeting)
 }
 
