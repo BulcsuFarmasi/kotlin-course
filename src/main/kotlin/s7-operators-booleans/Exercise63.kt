78d7cd814e3228f4e6b15f51907de672a77358f1
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-08-03 22:16:37 +0200

    exercise calculating interest

---
+++ src/main/kotlin/s7-operators-booleans/Exercise63.kt
@@ -0,0 +1,13 @@
+import kotlin.math.pow
+
+fun main(args:Array<String>) {
+    println("Please enter the amount in your  bank account")
+
+    val amountInput = readlnOrNull() ?: "0"
+    val amount = amountInput.toDouble()
+
+    val increasedAmount = amount *  1.055.pow(5)
+
+    println("Your amount after interest is: $increasedAmount")
+}
+
