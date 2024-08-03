2cd51655178e91dded3d722de192a28458ad8f88
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-08-03 22:03:36 +0200

    second and third exercises

--- src/main/kotlin/s7-operators-booleans/Practice62.kt
+++ src/main/kotlin/s7-operators-booleans/Practice62.kt
@@ -1,10 +1,23 @@
 fun main(args:Array<String>) {
-    var a = 76.254
-    var b = 3.867
+    // First exercise
+//    var a = 76.254
+//    var b = 3.867
+//
+//    println(a + b)
+//    println(a - b)
+//    println(a * b)
+//    println(a / b)
+//    println(a % b)
+// Second exercise
+//    var meatPricePerKg = 29.99
+//    var quantity = 1F / 3
+//    println("The price of the meat is: ${meatPricePerKg * quantity}")
+    // Third exercise
+    println("Please enter the amount in your bank account")
 
-    println(a + b)
-    println(a - b)
-    println(a * b)
-    println(a / b)
-    println(a % b)
+    val amountInput = readLine()?: "0"
+    val amount = amountInput.toDouble()
+
+    println("Congratulations, you now have ${amount * 3}")
+
 }
