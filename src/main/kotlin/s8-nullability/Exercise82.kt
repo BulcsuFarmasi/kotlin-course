b90968ffe7ea7c8204e720893a4d87bc6e6f90fb
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-08-17 20:54:13 +0200

    Exceptions

--- src/main/kotlin/s8-nullability/Exercise82.kt
+++ src/main/kotlin/s8-nullability/Exercise82.kt
@@ -1,2 +1,11 @@
-package `s8-nullability`
+fun main () {
+    val price = 29.99
+
+    println("The price of the product is $price, how many do you want to buy?")
+
+    val input = readLine()
+    val quantity = input?.toInt() ?: 2
+
+    println("The total price is ${price * quantity}")
+}
 
