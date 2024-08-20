d69e70512a3cb44e11e7e7a94ec4734a41b22735
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-08-20 17:24:57 +0200

    Exceptions

---
+++ src/main/kotlin/s9-exceptions/Finally.kt
@@ -0,0 +1,12 @@
+fun main () {
+    val test = readLine()
+    try {
+        println(test?.toInt())
+    } catch (e:Exception) {
+        e.printStackTrace()
+        println("An exception happened \n ${e.localizedMessage}")
+    } finally {
+        println("The execution has completed")
+    }
+}
+
