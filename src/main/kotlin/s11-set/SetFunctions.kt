c9f71a8fa438d58c5ec115c07400cd696e077012
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-09-09 20:25:06 +0200

    hash set functions

---
+++ src/main/kotlin/s11-set/SetFunctions.kt
@@ -0,0 +1,14 @@
+fun main () {
+    val numbers = setOf(3, 6, 4, null, 8)
+
+    println(numbers.size)
+
+    println(numbers.contains(4))
+    println(numbers.contains(5))
+
+
+    println(numbers.containsAll(setOf(4, 3)))
+
+    println(numbers.isEmpty())
+    println(setOf<Int>().isEmpty())
+}
