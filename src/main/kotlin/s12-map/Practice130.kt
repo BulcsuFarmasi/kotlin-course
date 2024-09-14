47277cb771e99df4d13df02a040035dc54511b6c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-09-14 19:32:47 +0200

    practice maps

---
+++ src/main/kotlin/s12-map/Practice130.kt
@@ -0,0 +1,15 @@
+fun main () {
+    var count = hashMapOf(Pair(1, "un"), Pair(2, "deux"), Pair(3, "trois"))
+
+    println("2 is in French is ${count[2]}")
+    println("3 is in French is ${count[3]}")
+
+    count.put(4, "quatre")
+    println(count)
+
+    val customers = hashMapOf(Pair("John", 1), Pair("Alice", 5), Pair("Bob", 3))
+
+    println("Is Alice a customer? : ${customers.containsKey("Alice")}")
+    println("Alice bought ${customers["Alice"]} products")
+    println("Is Michael a custumer? : ${customers.containsKey("Michael")}")
+}
