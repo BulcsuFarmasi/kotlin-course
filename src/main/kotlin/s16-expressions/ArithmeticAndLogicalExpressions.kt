b36079be6b45bf4e5c8f31aa34af34db7f03eac6
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-09-28 22:09:57 +0200

    arithmetic and logical expressions

---
+++ src/main/kotlin/s16-expressions/ArithmeticAndLogicalExpressions.kt
@@ -0,0 +1,20 @@
+fun main() {
+    if (326 / 72 * 15 + 93 % 12 == 65) {
+        println("Math is easy")
+    } else {
+        println("Math is hard")
+    }
+
+    val haveCat = true
+    val haveCatFood = false
+    if (haveCat && haveCatFood) {
+        println("Cat is happy")
+    } else {
+        println("Oh no!")
+    }
+
+    val age = 23
+
+    if (age >= 18) println("Can drink alcohol") else println("Illegal to drink alcohol")
+
+}
