b239fbbe70a9a8d20dabaa47bdd60dfd4cf2b152
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-09-30 21:21:58 +0200

    when statement

---
+++ src/main/kotlin/s17-when/WhenStatement.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val animal = "dog"
+    val action = when (animal) {
+        "cat" -> "pet it"
+        "dog" -> {
+            println("Yay I meet a dog")
+            "feed it"
+        }
+
+        else -> "google it"
+    }
+
+    println("If you meet a $animal you should $action")
+
+    val month = "April"
+    val days = when (month) {
+        "January", "March", "May" -> 31
+        "February" -> 28
+        else -> 30
+    }
+    println("The month of $month has $days days")
+}
