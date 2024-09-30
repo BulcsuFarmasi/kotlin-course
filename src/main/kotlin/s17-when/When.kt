a25fcd3d5604696a22d2ec71181d777409edf3fc
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-09-30 21:09:34 +0200

    when

---
+++ src/main/kotlin/s17-when/When.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val animal = "kangaroo"
+    var action = ""
+
+    when (animal) {
+        "cat" -> {
+            action = "pet it"
+        }
+
+        "dog" -> {
+            action = "feed it"
+        }
+
+        else -> {
+            action = "google it"
+        }
+    }
+
+    println("When you meet a $animal you should $action")
+
+    var result = ""
+    val number = 2344
+
+    when (number % 2) {
+        0 -> result = "Number is even"
+        1 -> result = "Number is odd"
+    }
+
+    println(result)
+}
