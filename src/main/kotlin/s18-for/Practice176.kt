aeb69c7ab079a073bf8ed5fd78f445be24de95f2
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-10-02 22:59:31 +0200

    practice extended loop

---
+++ src/main/kotlin/s18-for/Practice176.kt
@@ -0,0 +1,20 @@
+fun main() {
+//    println("Please input a number")
+//    val input = readlnOrNull()?:"0"
+//    val maxValue = input.toInt()
+//
+//    for (i in maxValue downTo 0) {
+//        if (i % 7 == 0) {
+//            println(i)
+//        }
+//    }
+
+    for (i in 1..10) {
+        for (j in 1..i) {
+            print("\uD83D\uDE42 ")
+        }
+        println()
+    }
+
+
+}
