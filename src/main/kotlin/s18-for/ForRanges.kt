0a42f936da674ee2106e4c0ea421ac6f5bb17273
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-10-02 21:24:06 +0200

    for loops for ranges

---
+++ src/main/kotlin/s18-for/ForRanges.kt
@@ -0,0 +1,20 @@
+fun main() {
+    for (i in 1..3) {
+        println("Strike $i")
+    }
+    println("You're out")
+
+    for (i in 10 downTo 0) {
+        println(i)
+        when (i) {
+            9 -> println("Start your engines")
+            6 -> println("on your marks")
+            3 -> println("Get set")
+            0 -> println("Go!")
+        }
+    }
+
+    for (i in 100 downTo -100 step 2) {
+        println(i)
+    }
+}
