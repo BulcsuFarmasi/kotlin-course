f24c2fae0116f9b54f8d5b6554f74aabb9c11a70
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-10-03 08:43:19 +0200

    exercise while loop

---
+++ src/main/kotlin/s19-while/Exercise183.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Please enter a large integer")
+    val input = readlnOrNull() ?: "0"
+    val number = input.toLong()
+
+    var index = 1L
+    while (index < number) {
+        if (index % 7L == 0L) {
+            println("$index is divisible by 7")
+        }
+        index++
+    }
+}
