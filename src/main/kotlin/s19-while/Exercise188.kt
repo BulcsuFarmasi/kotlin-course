6024d4c76ff3bbcdaef361d1fd9a8bdf72c38156
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-10-03 22:43:06 +0200

    exercise do while

---
+++ src/main/kotlin/s19-while/Exercise188.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val usernames = hashSetOf("john", "bob", "alice")
+
+    var finished = false
+
+    do {
+        println("Please enter a user name")
+        val username = readlnOrNull() ?: ""
+        if (username in usernames) {
+            println("The $username username is taken, please choose another")
+        } else {
+            usernames.add(username)
+            println("You've successfully choosen a username: $username")
+            finished = true
+        }
+    } while (!finished)
+    println(usernames)
+}
