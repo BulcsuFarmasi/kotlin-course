1f5f36784aede3eff8028d47b1dc153c573eb4cc
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-10-22 23:12:10 +0200

    Exercise vararg

---
+++ src/main/kotlin/s22-more-functions/Exercise221.kt
@@ -0,0 +1,11 @@
+fun main() {
+    hello(4, "John", "Abraham", "Mike", "Oscar", "Carl")
+}
+
+fun hello(count: Int, vararg clients: String) {
+    for (client in clients) {
+        for (i in 1..count) {
+            println("Hello $client")
+        }
+    }
+}
