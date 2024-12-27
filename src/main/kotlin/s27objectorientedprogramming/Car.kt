c89c97ac00f36eb45c3917ff16638fd72c8b4e96
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2024-12-27 20:12:14 +0100

    create object from class

--- src/main/kotlin/s27objectorientedprogramming/Car.kt
+++ src/main/kotlin/s27objectorientedprogramming/Car.kt
@@ -1,7 +1,18 @@
 package s27objectorientedprogramming
 
 fun main() {
+    val myCar: Car = Car()
+    val yourCar = Car()
 
+    myCar.model = "BMW"
+    yourCar.model = "Toyota"
+    myCar.topSpeed = 150
+
+    myCar.start()
+    myCar.drive(50)
+
+    yourCar.start()
+    yourCar.drive(75)
 }
 
 class Car {
