eaedf53188bcbd8cdaf29710ab0761d456407645
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-01-05 14:37:57 +0100

    getters and setters

---
+++ src/main/kotlin/s28moreclasses/GetterSetter.kt
@@ -0,0 +1,17 @@
+package s28moreclasses
+
+fun main() {
+    val car = WeirdCar()
+    car.speed = 100
+    println("car.name: ${car.name}")
+    println("car.speed: ${car.speed}")
+}
+
+class WeirdCar {
+    var name = ""
+    var speed: Int
+        get() = 50
+        set(value) {
+            name = "High speed car $value"
+        }
+}
