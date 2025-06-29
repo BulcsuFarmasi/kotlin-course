b3c38872f82d179399c97cd981f08940e6c638ab
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2025-06-29 22:54:54 +0200

    exercise more interfaces

--- src/main/kotlin/s30interfaces/Exercise328.kt
+++ src/main/kotlin/s30interfaces/Exercise328.kt
@@ -1,2 +1,40 @@
 package s30interfaces
 
+fun main() {
+    var car = CarRental().getUtilityCar()
+    car.drive()
+
+    car = CarRental().getLimousine()
+    car.drive()
+}
+
+interface Auto {
+    fun drive()
+}
+
+class UtilityCar : Auto {
+    override fun drive() {
+        println("Utility car drives you to the destination")
+    }
+
+}
+
+class Limousine : Auto {
+    override fun drive() {
+        println("Limousine gets you to your destination in comfort")
+    }
+
+}
+
+class CarRental {
+    val priceForUtilityCar = 100
+    val priceForLimousine = 500
+
+    fun getUtilityCar(): Auto {
+        return UtilityCar()
+    }
+
+    fun getLimousine(): Auto {
+        return Limousine()
+    }
+}
